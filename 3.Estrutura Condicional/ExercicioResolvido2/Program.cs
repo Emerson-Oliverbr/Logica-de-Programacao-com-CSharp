9e6f47cf8247b627d13c886f1d324397c3dddd00
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-05-14 19:30:46 -0300

    Add: Exercicios de exemplo 2 e 3

---
+++ 3.Estrutura Condicional/ExercicioResolvido2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioResolvido2
+{
+	internal class Program
+	{ 
+		static void Main(string[] args)
+		{
+			  double nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+			  double nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+			  double media = nota1 + nota2;
+
+			  if(media > 60.0) {
+			  		Console.WriteLine($"NOTA FINAL = {media.ToString("F1",CultureInfo.InvariantCulture)}");		
+			  }
+			  else{
+			  		Console.WriteLine($"NOTA FINAL {media.ToString("F1",CultureInfo.InvariantCulture)}");
+			  		Console.WriteLine($"REPROVADO");
+			  }
+		}
+	}
+}
---
+++ 3.Estrutura Condicional/ExercicioResolvido3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercicioResolvido3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] valores = Console.ReadLine().Split(' ');
+			int valor1 = int.Parse(valores[0]);
+			int valor2 = int.Parse(valores[1]);
+			int valor3 = int.Parse(valores[2]);
+
+			int menor;
+
+			if(valor1 < valor2 && valor1 < valor3) {
+				menor = valor1;
+			}
+			else if (valor2 < valor1 && valor2 < valor3) {
+				menor = valor2;
+			}
+			else {
+				menor = valor3;
+			}
+
+			Console.WriteLine($"MENOR = {menor}");
+		}
+	}
+	
+}
