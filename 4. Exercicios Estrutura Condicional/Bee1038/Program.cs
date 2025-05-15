fe78429ad085d0e9bc23c83901e32e42e0d982e6
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-05-14 21:31:21 -0300

    Add: primeira parte dos exercicios

---
+++ 4. Exercicios Estrutura Condicional/Bee1038/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+internal class Bee1038
+{
+	internal class Program
+	{ 
+		static void Main(string[] args)
+		{ 
+			string[] opcoes = Console.ReadLine().Split(' ');
+			int codigo = int.Parse(opcoes[0]);
+			int quantidade = int.Parse(opcoes[1]);
+
+			double totalAPagar = 0.0;
+
+			if(codigo == 1) 
+			{
+				totalAPagar = quantidade * 4.00;
+			}
+			else if(codigo == 2)
+			{
+				totalAPagar = quantidade * 4.50;
+			}
+			else if(codigo == 3)
+			{
+				totalAPagar = quantidade * 5.00;
+			}
+			else if(codigo == 4)
+			{
+				totalAPagar = quantidade * 2.00;
+			}
+			else if(codigo == 5)
+			{
+				totalAPagar = quantidade * 1.50;
+			}
+			else 
+			{
+				Console.WriteLine("Codigo invalido");
+			}
+
+			Console.WriteLine($"Total: R$ {totalAPagar.ToString("F2",CultureInfo.InvariantCulture)}");
+		}
+	}
+}
---
+++ 4. Exercicios Estrutura Condicional/Bee1044/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bee1044
+{
+	internal class Bee1044
+	{ 
+		static void Main(string[] args)
+		{
+			 string[] valores = Console.ReadLine().Split(' ');
+			 int valor1 = int.Parse(valores[0]);
+			 int valor2 = int.Parse(valores[1]);
+
+			 if(valor1 % valor2 == 0 || valor2 % valor1 == 0) 
+			 {	
+			 	 Console.WriteLine("Sao Multiplos");
+			 }
+			 else 
+			 { 
+			 	 Console.WriteLine("Nao sao Multiplos");
+			 }
+		}
+	} 
+}
