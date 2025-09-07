893450db6581b06b69373eefd6e89918658be0fe
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-07 18:05:57 -0300

    Entrada de dados primeira parte

---
+++ Modulo-1-Estrutura Sequencial/EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace EntradaDeDados;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string frase = Console.ReadLine();
+        Console.WriteLine("Voce digitou: " + frase);
+
+        string cor1 = Console.ReadLine();
+        string cor2 = Console.ReadLine();
+        string cor3 = Console.ReadLine();
+
+        Console.WriteLine("Cores digitadas: ");
+        Console.WriteLine(cor1);
+        Console.WriteLine(cor2);
+        Console.WriteLine(cor3);
+
+        string frutas = Console.ReadLine();
+
+        string[] minhasFrutas = frutas.Split(' ');
+        string fruta1 = minhasFrutas[0];
+        string fruta2 = minhasFrutas[1];
+        string fruta3 = minhasFrutas[2];
+
+        Console.WriteLine(fruta1);
+        Console.WriteLine(fruta2);
+        Console.WriteLine(fruta3);
+    }
+}
+
