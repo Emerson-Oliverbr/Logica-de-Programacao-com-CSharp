d32494565cc25472d25558a1aca796608f2caddb
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-28 22:38:01 -0300

    Iniciando o modulo 3 Estrutura Condicional

---
+++ Modulo-2-EstruturaCondicional/Exemplo1/Exemplo1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exemplo1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int horas = 5;
+
+        Console.WriteLine("Que horas são agora? ");
+        horas = int.Parse(Console.ReadLine());
+
+        if (horas < 12)
+        {
+            Console.WriteLine("Bom dia");
+        }
+        else if(horas > 18)
+        {
+            Console.WriteLine("Boa noite");
+        }
+        else
+        {
+            Console.WriteLine("Boa tarde");
+        }
+    }
+}
