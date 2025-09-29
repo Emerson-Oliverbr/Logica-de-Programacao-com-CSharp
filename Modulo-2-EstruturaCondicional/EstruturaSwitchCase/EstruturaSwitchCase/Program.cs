0f0ea6ee8495d8790a0c19504f464a98b33b8438
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-09-29 20:39:39 -0300

    Iniciando o modulo sobre estrututa condicional

---
+++ Modulo-2-EstruturaCondicional/EstruturaSwitchCase/EstruturaSwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace EstruturaSwitchCase;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+
+        string dia = null;
+
+        switch (x)
+        {
+            case 1:
+                dia = "Domingo"; 
+                break;
+            case 2:
+                dia = "Segunda"; 
+                break;
+            case 3:
+                dia = "Terça"; 
+                break;
+            case 4:
+                dia = "Quarta"; 
+                break;
+            case 5:
+                dia = "Quinta"; 
+                break;
+            case 6:
+                dia = "Sexta"; 
+                break;
+            case 7:
+                dia = "Sabado"; 
+                break;
+            default:
+                dia = "Dia invalido";
+                break;
+        }
+
+        Console.WriteLine($"Dia da semana: {dia}");
+    }
+}
---
+++ Modulo-2-EstruturaCondicional/ExercicioResolvido1/ExercicioResolvido1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExercicioResolvido1;
+
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        double nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        double media = nota1 + nota2;
+
+        if (media >= 60.0)
+        {
+            Console.WriteLine($"Media Final {media.ToString("F1",CultureInfo.InvariantCulture)}");
+        }
+        else
+        {
+            Console.WriteLine($"Nota Final {media.ToString("F1",CultureInfo.InvariantCulture)}");
+            Console.WriteLine("Reprovado");
+        }
+    }
+}
