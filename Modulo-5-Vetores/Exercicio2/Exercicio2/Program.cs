97bfb5ed8a9c0add21baddbb31cd07ffef07d08a
Emerson Oliveira <emerson.oliverbr@gmail.com>
2025-10-14 11:38:58 -0300

    usando o foreach

---
+++ Modulo-5-Vetores/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercicio2;
+using System.Globalization; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        double[] numeros = new double[n];
+
+        string[] valores = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < n; i++)
+        {
+            numeros[i] = double.Parse(valores[i],CultureInfo.InvariantCulture);
+        }
+        Console.WriteLine();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(numeros[i].ToString("F1",CultureInfo.InvariantCulture) + " ");
+        }
+
+        int quantidade = 0;
+        double totalValores = 0.0;
+        double media = 0.0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            numeros[i] = double.Parse(valores[i],CultureInfo.InvariantCulture);
+            quantidade += 1;
+            totalValores += numeros[i];
+            media = (double) totalValores / quantidade;
+        }
+        Console.WriteLine();
+        Console.WriteLine(totalValores.ToString("F2",CultureInfo.InvariantCulture));
+        Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+    }
+}
---
+++ Modulo-5-Vetores/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercicio3;
+
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        string[] nomes = new string[n];
+        int[] idades = new int[n];
+        double[] alturas = new double[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] dadosPessoais = Console.ReadLine().Split(' ');
+            nomes[i] = dadosPessoais[0];
+            idades[i] = int.Parse(dadosPessoais[1]);
+            alturas[i] = double.Parse(dadosPessoais[2],CultureInfo.InvariantCulture);
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            soma += alturas[i];
+        }
+        double media = soma / n;
+        Console.WriteLine($"Altura media: {media.ToString("F2",CultureInfo.InvariantCulture)}");
+
+        int quantidadeDePessoas = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (idades[i] < 16)
+            {
+                quantidadeDePessoas++;
+            }
+        }
+        double porcentagem = (double) quantidadeDePessoas / n * 100.0;
+        Console.WriteLine($"Pessoas com menos de 16 anos: {porcentagem.ToString("F1",CultureInfo.InvariantCulture)} %");
+    }
+}
